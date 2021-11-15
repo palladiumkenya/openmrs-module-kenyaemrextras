2b73290183144190fb3cfd3f7332491f6387789e
ojwanganto <ojwangwachiaje@gmail.com>
2021-11-16 00:14:18 +0300

    add dqa report

---
+++ api/src/main/java/org/openmrs/module/kenyaemrextras/reporting/data/definition/evaluator/LastNutritionAssessmentDataEvaluator.java
@@ -0,0 +1,52 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.kenyaemrextras.reporting.data.definition.evaluator;
+
+import org.openmrs.annotation.Handler;
+import org.openmrs.module.kenyaemrextras.reporting.data.definition.LastNutritionAssessmentDataDefinition;
+import org.openmrs.module.reporting.data.person.EvaluatedPersonData;
+import org.openmrs.module.reporting.data.person.definition.PersonDataDefinition;
+import org.openmrs.module.reporting.data.person.evaluator.PersonDataEvaluator;
+import org.openmrs.module.reporting.evaluation.EvaluationContext;
+import org.openmrs.module.reporting.evaluation.EvaluationException;
+import org.openmrs.module.reporting.evaluation.querybuilder.SqlQueryBuilder;
+import org.openmrs.module.reporting.evaluation.service.EvaluationService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Map;
+
+/**
+ * Evaluates nutrition assessment Data Definition
+ */
+@Handler(supports = LastNutritionAssessmentDataDefinition.class, order = 50)
+public class LastNutritionAssessmentDataEvaluator implements PersonDataEvaluator {
+	
+	@Autowired
+	private EvaluationService evaluationService;
+	
+	public EvaluatedPersonData evaluate(PersonDataDefinition definition, EvaluationContext context)
+	        throws EvaluationException {
+		EvaluatedPersonData c = new EvaluatedPersonData(definition, context);
+		
+		String qry = "select fup.patient_id,\n" + "case mid(max(concat(fup.visit_date, fup.nutritional_status)), 11)\n"
+		        + "              when 1115 then \"Normal\"\n"
+		        + "              when 163302 then \"Severe acute malnutrition\"\n"
+		        + "              when 163303 then \"Moderate acute malnutrition\"\n"
+		        + "              when 114413 then \"Overweight/Obese\"\n"
+		        + "              else \"\" end as nutrition_assessment from kenyaemr_etl.etl_patient_hiv_followup fup\n"
+		        + "\tGROUP BY fup.patient_id;";
+		
+		SqlQueryBuilder queryBuilder = new SqlQueryBuilder();
+		queryBuilder.append(qry);
+		Map<Integer, Object> data = evaluationService.evaluateToMap(queryBuilder, Integer.class, Object.class, context);
+		c.setData(data);
+		return c;
+	}
+}
