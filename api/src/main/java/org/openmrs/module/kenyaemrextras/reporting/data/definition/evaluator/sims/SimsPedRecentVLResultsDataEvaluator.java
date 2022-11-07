cefc31ae7999e38d87442b8e163e03e1f58549ae
Makombe <makombe>
2022-11-07 23:33:26 +0300

    Recent Vl test ordered within appropriate time

--- api/src/main/java/org/openmrs/module/kenyaemrextras/reporting/data/definition/evaluator/sims/SimsPedRecentVLResultsDataEvaluator.java
+++ api/src/main/java/org/openmrs/module/kenyaemrextras/reporting/data/definition/evaluator/sims/SimsPedRecentVLResultsDataEvaluator.java
@@ -10,7 +10,8 @@
 package org.openmrs.module.kenyaemrextras.reporting.data.definition.evaluator.sims;
 
 import org.openmrs.annotation.Handler;
-import org.openmrs.module.kenyaemrextras.reporting.data.definition.sims.SimsScreenedPostiveForCervicalCancerDataDefinition;
+import org.openmrs.module.kenyaemrextras.reporting.data.definition.sims.SimsPedsRecentVLResultsDataDefinition;
+import org.openmrs.module.kenyaemrextras.reporting.data.definition.sims.SimsRecentVLResultsDataDefinition;
 import org.openmrs.module.reporting.data.person.EvaluatedPersonData;
 import org.openmrs.module.reporting.data.person.definition.PersonDataDefinition;
 import org.openmrs.module.reporting.data.person.evaluator.PersonDataEvaluator;
@@ -24,10 +25,10 @@ import java.util.Date;
 import java.util.Map;
 
 /**
- * Evaluates
+ * Evaluates whether ped has most recent viral load results documented
  */
-@Handler(supports = SimsScreenedPostiveForCervicalCancerDataDefinition.class, order = 50)
-public class SimsScreenedPositiveForCervicalCancerDataEvaluator implements PersonDataEvaluator {
+@Handler(supports = SimsPedsRecentVLResultsDataDefinition.class, order = 50)
+public class SimsPedRecentVLResultsDataEvaluator implements PersonDataEvaluator {
 	
 	@Autowired
 	private EvaluationService evaluationService;
@@ -36,10 +37,29 @@ public class SimsScreenedPositiveForCervicalCancerDataEvaluator implements Perso
 	        throws EvaluationException {
 		EvaluatedPersonData c = new EvaluatedPersonData(definition, context);
 		
-		String qry = "select cs.patient_id,if(cs.screening_result =\"Negative\",'NA', if(cs.screening_result =\"Positive\" and cs.treatment_method is not null, 'Y','N'))\n"
-		        + "from kenyaemr_etl.etl_cervical_cancer_screening cs \n"
-		        + "where  cs.visit_date between date_sub(date(:endDate) , interval 90 DAY) and date(:endDate)\n"
-		        + "GROUP BY cs.patient_id";
+		String qry = "\n"
+		        + "select patient_id, if(t.lastVL is not null , 'Y','N')\n"
+		        + "  \n"
+		        + "  from (\n"
+		        + "select d.patient_id, d.dob as dob,\n"
+		        + "mid(max(concat(l.visit_date, l.test_result)), 11)  as lastVL,\n"
+		        + "mid(max(concat(l.visit_date, l.date_test_requested)), 11)  as dateTestOrdered,\n"
+		        + "mid(max(concat(l.visit_date, l.date_test_result_received)), 11)  as dateResultReceived\n"
+		        + "\n"
+		        + "from kenyaemr_etl.etl_patient_demographics d\n"
+		        + "left join (\n"
+		        + "select x.patient_id, x.visit_date ,x.test_result, x.date_test_requested, x.date_test_result_received\n"
+		        + "  from kenyaemr_etl.etl_laboratory_extract x\n"
+		        + "  where  lab_test in (856, 1305)\n"
+		        + "  GROUP BY  x.patient_id\n"
+		        + "\n"
+		        + ") l on d.patient_id = l.patient_id\n"
+		        + "group by d.patient_id\n"
+		        + ")t\n"
+		        + "where (\n"
+		        + "  t.lastVL is not null  and (lastVL > 1000 and lastVL!=1302 and timestampdiff(MONTH,t.dateTestOrdered, \"2022-10-30\") <= 3)\n"
+		        + "  or  t.lastVL is not null  and (lastVL < 1000 or lastVL=1302) and (timestampdiff(YEAR,t.dob,\"2022-10-30\"))<15 and  timestampdiff(MONTH,t.dateTestOrdered, \"2022-10-30\") <6\n"
+		        + "\n" + ")";
 		
 		SqlQueryBuilder queryBuilder = new SqlQueryBuilder();
 		Date startDate = (Date) context.getParameterValue("startDate");
--- api/src/main/java/org/openmrs/module/kenyaemrextras/reporting/data/definition/evaluator/sims/SimsPedsRecentVLTestOrderedDataEvaluator.java
+++ api/src/main/java/org/openmrs/module/kenyaemrextras/reporting/data/definition/evaluator/sims/SimsPedsRecentVLTestOrderedDataEvaluator.java
@@ -10,7 +10,7 @@
 package org.openmrs.module.kenyaemrextras.reporting.data.definition.evaluator.sims;
 
 import org.openmrs.annotation.Handler;
-import org.openmrs.module.kenyaemrextras.reporting.data.definition.sims.SimsScreenedPostiveForCervicalCancerDataDefinition;
+import org.openmrs.module.kenyaemrextras.reporting.data.definition.sims.SimsPedsRecentVLTestOrderedDataDefinition;
 import org.openmrs.module.reporting.data.person.EvaluatedPersonData;
 import org.openmrs.module.reporting.data.person.definition.PersonDataDefinition;
 import org.openmrs.module.reporting.data.person.evaluator.PersonDataEvaluator;
@@ -24,10 +24,11 @@ import java.util.Date;
 import java.util.Map;
 
 /**
- * Evaluates
+ * Evaluates whether most recent viral load test was ordered within the appropriate interval per the
+ * national guidelines
  */
-@Handler(supports = SimsScreenedPostiveForCervicalCancerDataDefinition.class, order = 50)
-public class SimsScreenedPositiveForCervicalCancerDataEvaluator implements PersonDataEvaluator {
+@Handler(supports = SimsPedsRecentVLTestOrderedDataDefinition.class, order = 50)
+public class SimsPedsRecentVLTestOrderedDataEvaluator implements PersonDataEvaluator {
 	
 	@Autowired
 	private EvaluationService evaluationService;
@@ -36,10 +37,27 @@ public class SimsScreenedPositiveForCervicalCancerDataEvaluator implements Perso
 	        throws EvaluationException {
 		EvaluatedPersonData c = new EvaluatedPersonData(definition, context);
 		
-		String qry = "select cs.patient_id,if(cs.screening_result =\"Negative\",'NA', if(cs.screening_result =\"Positive\" and cs.treatment_method is not null, 'Y','N'))\n"
-		        + "from kenyaemr_etl.etl_cervical_cancer_screening cs \n"
-		        + "where  cs.visit_date between date_sub(date(:endDate) , interval 90 DAY) and date(:endDate)\n"
-		        + "GROUP BY cs.patient_id";
+		String qry = "select patient_id, if((lastVL is not null  and (lastVL > 1000 and lastVL!=1302 and timestampdiff(MONTH,dateTestOrdered, :endDate) <= 3)\n"
+		        + "  or  lastVL is not null  and (lastVL < 1000 or lastVL=1302) and (timestampdiff(YEAR,dob,:endDate))<25 and  timestampdiff(MONTH,dateTestOrdered, :endDate) <=6\n"
+		        + "  ),'Y','N') from (\n"
+		        + "select d.patient_id, d.dob as dob,\n"
+		        + "mid(max(concat(l.visit_date, l.test_result)), 11)  as lastVL,\n"
+		        + "mid(max(concat(l.visit_date, l.date_test_requested)), 11)  as dateTestOrdered\n"
+		        + "\n"
+		        + "from kenyaemr_etl.etl_patient_demographics d\n"
+		        + "left join (\n"
+		        + "select x.patient_id, x.visit_date ,x.test_result, x.date_test_requested\n"
+		        + "  from kenyaemr_etl.etl_laboratory_extract x\n"
+		        + "  where  lab_test in (856, 1305)\n"
+		        + "  GROUP BY  x.patient_id\n"
+		        + "\n"
+		        + ") l on d.patient_id = l.patient_id\n"
+		        + "group by d.patient_id\n"
+		        + ")t\n"
+		        + "where (\n"
+		        + "  t.lastVL is not null  and (lastVL > 1000 and lastVL!=1302 and timestampdiff(MONTH,t.dateTestOrdered, :endDate) <= 3)\n"
+		        + "  or  t.lastVL is not null  and (lastVL < 1000 or lastVL=1302) and (timestampdiff(YEAR,t.dob,:endDate))<25 and  timestampdiff(MONTH,t.dateTestOrdered, :endDate) <=6\n"
+		        + ")";
 		
 		SqlQueryBuilder queryBuilder = new SqlQueryBuilder();
 		Date startDate = (Date) context.getParameterValue("startDate");
