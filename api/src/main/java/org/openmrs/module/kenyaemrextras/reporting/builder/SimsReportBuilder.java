d544472aa7c456b8e3233d09d4026286d37d3653
Stephen Chege <njorocs@gmail.com>
2022-11-16 13:49:07 +0300

    KHP3-2489: S_03_10

--- api/src/main/java/org/openmrs/module/kenyaemrextras/reporting/builder/SimsReportBuilder.java
+++ api/src/main/java/org/openmrs/module/kenyaemrextras/reporting/builder/SimsReportBuilder.java
@@ -97,6 +97,7 @@ public class SimsReportBuilder extends AbstractHybridReportBuilder {
 		DataSetDefinition txCurrKpWithTestedContactsDSD = txCurrKpWithTestedContactsDatasetDefinition("S_03_14");
 		DataSetDefinition txCurrKpWithTestedChildContactsDSD = txCurrKpWithTestedChildContactsDatasetDefinition("S_03_15");
 		DataSetDefinition txCurrKpWithTBScreeningResultDSD = txCurrKpWithTBScreeningResultDatasetDefinition("S_03_16");
+		DataSetDefinition txCurrKPsRecentPositivesARTInitiationDSD = txCurrKPsRecentPositivesARTInitiationDatasetDefinition("S_03_10");
 		
 		return Arrays.asList(ReportUtils.map(newlyInitiatedOnArtPatientsDSD, "startDate=${startDate},endDate=${endDate}"),
 		    ReportUtils.map(missedAppointmentsDSD, "startDate=${startDate},endDate=${endDate}"),
@@ -121,7 +122,8 @@ public class SimsReportBuilder extends AbstractHybridReportBuilder {
 		    ReportUtils.map(txCurrKpNonVirallySuppressedDSD, "startDate=${startDate},endDate=${endDate}"),
 		    ReportUtils.map(txCurrKpWithTestedContactsDSD, "startDate=${startDate},endDate=${endDate}"),
 		    ReportUtils.map(txCurrKpWithTestedChildContactsDSD, "startDate=${startDate},endDate=${endDate}"),
-		    ReportUtils.map(txCurrKpWithTBScreeningResultDSD, "startDate=${startDate},endDate=${endDate}")
+		    ReportUtils.map(txCurrKpWithTBScreeningResultDSD, "startDate=${startDate},endDate=${endDate}"),
+		    ReportUtils.map(txCurrKPsRecentPositivesARTInitiationDSD, "startDate=${startDate},endDate=${endDate}")
 		
 		);
 		
@@ -1135,6 +1137,48 @@ public class SimsReportBuilder extends AbstractHybridReportBuilder {
 		cd.setName("KP Adults on ART");
 		dsd.addRowFilter(cd, indParams);
 		
+		return dsd;
+	}
+	
+	/**
+	 * Tx_Curr KPs with clinical visits within the last 3 months who recently turned HIV+ and their
+	 * ART initiation documentation
+	 * 
+	 * @param datasetName
+	 * @return
+	 */
+	protected PatientDataSetDefinition txCurrKPsRecentPositivesARTInitiationDatasetDefinition(String datasetName) {
+		
+		PatientDataSetDefinition dsd = new PatientDataSetDefinition(datasetName);
+		String indParams = "startDate=${startDate},endDate=${endDate}";
+		
+		dsd.addParameter(new Parameter("startDate", "Start Date", Date.class));
+		dsd.addParameter(new Parameter("endDate", "End Date", Date.class));
+		
+		PatientIdentifierType upn = MetadataUtils.existing(PatientIdentifierType.class,
+		    HivMetadata._PatientIdentifierType.UNIQUE_PATIENT_NUMBER);
+		DataConverter identifierFormatter = new ObjectFormatter("{identifier}");
+		DataDefinition identifierDef = new ConvertedPatientDataDefinition("identifier", new PatientIdentifierDataDefinition(
+		        upn.getName(), upn), identifierFormatter);
+		
+		DataConverter nameFormatter = new ObjectFormatter("{familyName}, {givenName}");
+		DataDefinition nameDef = new ConvertedPersonDataDefinition("name", new PreferredNameDataDefinition(), nameFormatter);
+		
+		SimsTxNewKPARTInitiationDocumentationStatusDataDefinition kpTxCurrNewPositivesRapidARTInitiationDataDefinition = new SimsTxNewKPARTInitiationDocumentationStatusDataDefinition();
+		kpTxCurrNewPositivesRapidARTInitiationDataDefinition.addParameter(new Parameter("startDate", "Start Date",
+		        Date.class));
+		kpTxCurrNewPositivesRapidARTInitiationDataDefinition.addParameter(new Parameter("endDate", "End Date", Date.class));
+		
+		dsd.addColumn("id", new PersonIdDataDefinition(), "");
+		dsd.addColumn("Name", nameDef, "");
+		dsd.addColumn("CCC No", identifierDef, "");
+		dsd.addColumn("Sex", new GenderDataDefinition(), "", null);
+		dsd.addColumn("Date of Birth", new BirthdateDataDefinition(), "", new BirthdateConverter(DATE_FORMAT));
+		dsd.addColumn("S_03_10 Q2", kpTxCurrNewPositivesRapidARTInitiationDataDefinition, indParams, null);
+		CohortDefinition cd = new S0310CohortDefinition();
+		cd.addParameter(new Parameter("startDate", "Start Date", Date.class));
+		cd.addParameter(new Parameter("endDate", "End Date", Date.class));
+		dsd.addRowFilter(cd, indParams);
 		return dsd;
 	}
 }
