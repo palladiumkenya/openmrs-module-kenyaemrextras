639e348b054b356ea64227399b23d798e8bddf92
Patrick <gpatryllus@gmail.com>
2022-11-21 17:40:42 +0300

    Addded sheet S_04_08 to S_04_14

---
+++ api/src/main/java/org/openmrs/module/kenyaemrextras/reporting/data/definition/evaluator/sims/SimsMotherAndBabyProphylaxisGivenMaternityDataEvaluator.java
@@ -0,0 +1,56 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.kenyaemrextras.reporting.data.definition.evaluator.sims;
+
+import org.openmrs.annotation.Handler;
+import org.openmrs.module.kenyaemrextras.reporting.data.definition.sims.SimsMotherAndBabyProphylaxisGivenMaternityDataDefinition;
+import org.openmrs.module.kenyaemrextras.reporting.data.definition.sims.SimsTestedWithin3MonthsOfMaternityDataDefinition;
+import org.openmrs.module.reporting.data.person.EvaluatedPersonData;
+import org.openmrs.module.reporting.data.person.definition.PersonDataDefinition;
+import org.openmrs.module.reporting.data.person.evaluator.PersonDataEvaluator;
+import org.openmrs.module.reporting.evaluation.EvaluationContext;
+import org.openmrs.module.reporting.evaluation.EvaluationException;
+import org.openmrs.module.reporting.evaluation.querybuilder.SqlQueryBuilder;
+import org.openmrs.module.reporting.evaluation.service.EvaluationService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+import java.util.Map;
+
+/**
+ * Tested Mother and Baby prophylaxis given at Maternity
+ */
+@Handler(supports = SimsMotherAndBabyProphylaxisGivenMaternityDataDefinition.class, order = 50)
+public class SimsMotherAndBabyProphylaxisGivenMaternityDataEvaluator implements PersonDataEvaluator {
+	
+	@Autowired
+	private EvaluationService evaluationService;
+	
+	public EvaluatedPersonData evaluate(PersonDataDefinition definition, EvaluationContext context)
+	        throws EvaluationException {
+		EvaluatedPersonData c = new EvaluatedPersonData(definition, context);
+		
+		String qry = "select ld.patient_id, case mid(max(concat(ld.visit_date, ld.prophylaxis_given)), 11)\n"
+		        + "               when 105281 then 'Y' \n"
+		        + "               when 74250 then 'Y'  when 1107 then 'N' when 1175 then 'N/A'\n"
+		        + "               else '' end as prophylaxis_given from kenyaemr_etl.etl_mchs_delivery ld\n"
+		        + " where date(ld.visit_date) <= date(:endDate) GROUP BY ld.patient_id;";
+		
+		SqlQueryBuilder queryBuilder = new SqlQueryBuilder();
+		Date startDate = (Date) context.getParameterValue("startDate");
+		Date endDate = (Date) context.getParameterValue("endDate");
+		queryBuilder.addParameter("endDate", endDate);
+		queryBuilder.addParameter("startDate", startDate);
+		queryBuilder.append(qry);
+		Map<Integer, Object> data = evaluationService.evaluateToMap(queryBuilder, Integer.class, Object.class, context);
+		c.setData(data);
+		return c;
+	}
+}
