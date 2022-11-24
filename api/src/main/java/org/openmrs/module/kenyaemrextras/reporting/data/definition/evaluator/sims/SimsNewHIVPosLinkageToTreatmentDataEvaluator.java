0664029b86cbacca8ea857db0791b2438ba8b633
Stephen Chege <njorocs@gmail.com>
2022-11-24 16:11:31 +0300

    KHP3-2518:S_08_02, KHP3-2519:S_07_03. Added HEI ID identifer

---
+++ api/src/main/java/org/openmrs/module/kenyaemrextras/reporting/data/definition/evaluator/sims/SimsNewHIVPosLinkageToTreatmentDataEvaluator.java
@@ -0,0 +1,56 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ * <p>
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.kenyaemrextras.reporting.data.definition.evaluator.sims;
+
+import org.openmrs.annotation.Handler;
+import org.openmrs.module.kenyaemrextras.reporting.data.definition.sims.SimsNewHIVPosLinkageToTreatmentDataDefinition;
+import org.openmrs.module.reporting.data.person.EvaluatedPersonData;
+import org.openmrs.module.reporting.data.person.definition.PersonDataDefinition;
+import org.openmrs.module.reporting.data.person.evaluator.PersonDataEvaluator;
+import org.openmrs.module.reporting.evaluation.EvaluationContext;
+import org.openmrs.module.reporting.evaluation.EvaluationException;
+import org.openmrs.module.reporting.evaluation.querybuilder.SqlQueryBuilder;
+import org.openmrs.module.reporting.evaluation.service.EvaluationService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+import java.util.Map;
+
+/**
+ * S_07_03 Q4: HTS Linkage to HIV Care and Treatment at the Site Level: Does the chart record
+ * successful linkage to treatment/ART?
+ */
+@Handler(supports = SimsNewHIVPosLinkageToTreatmentDataDefinition.class, order = 50)
+public class SimsNewHIVPosLinkageToTreatmentDataEvaluator implements PersonDataEvaluator {
+	
+	@Autowired
+	private EvaluationService evaluationService;
+	
+	public EvaluatedPersonData evaluate(PersonDataDefinition definition, EvaluationContext context)
+	        throws EvaluationException {
+		EvaluatedPersonData c = new EvaluatedPersonData(definition, context);
+		
+		String qry = "select e.patient_id,if(d.art_client is not null,'Y','N') as is_on_art\n"
+		        + "from kenyaemr_etl.etl_tb_enrollment e\n" + "         left join\n"
+		        + "     (select d.patient_id as art_client\n" + "      from kenyaemr_etl.etl_drug_event d\n"
+		        + "      where d.date_started <= date(:endDate)\n" + "        and d.program = 'HIV'\n"
+		        + "        and d.discontinued is null) d on e.patient_id = d.art_client;";
+		
+		SqlQueryBuilder queryBuilder = new SqlQueryBuilder();
+		Date startDate = (Date) context.getParameterValue("startDate");
+		Date endDate = (Date) context.getParameterValue("endDate");
+		queryBuilder.addParameter("endDate", endDate);
+		queryBuilder.addParameter("startDate", startDate);
+		queryBuilder.append(qry);
+		Map<Integer, Object> data = evaluationService.evaluateToMap(queryBuilder, Integer.class, Object.class, context);
+		c.setData(data);
+		return c;
+	}
+}
