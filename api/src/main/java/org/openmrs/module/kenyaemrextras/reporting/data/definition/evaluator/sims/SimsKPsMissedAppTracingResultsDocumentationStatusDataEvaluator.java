7f476a97a4ce1ec8d9b55d89e5daf7247d6006f7
Stephen Chege <njorocs@gmail.com>
2022-11-25 16:23:16 +0300

    Fixed bug in S_07_03 Q4 query and corrected mapping for S_04_15 and S_04_17.

--- api/src/main/java/org/openmrs/module/kenyaemrextras/reporting/data/definition/evaluator/sims/SimsKPsMissedAppTracingResultsDocumentationStatusDataEvaluator.java
+++ api/src/main/java/org/openmrs/module/kenyaemrextras/reporting/data/definition/evaluator/sims/SimsKPsMissedAppTracingResultsDocumentationStatusDataEvaluator.java
@@ -36,21 +36,20 @@ public class SimsKPsMissedAppTracingResultsDocumentationStatusDataEvaluator impl
 	        throws EvaluationException {
 		EvaluatedPersonData c = new EvaluatedPersonData(definition, context);
 		
-		String qry = "select a.patient_id, if(a.tracing_outcome <> '', 'Y', 'N')\n" +
-				"from (select f.patient_id,\n" +
-				"             max(date(f.visit_date))                                     as latest_fup_visit,\n" +
-				"             mid(max(concat(f.visit_date, f.next_appointment_date)), 11) as latest_app_date,\n" +
-				"             t.tracking_date,\n" +
-				"             t.tracing_outcome\n" +
-				"      from kenyaemr_etl.etl_patient_hiv_followup f\n" +
-				"               left join (select t.client_id,\n" +
-				"                                 max(t.visit_date)                                     as tracking_date,\n" +
-				"                                 mid(max(concat(t.visit_date, t.tracing_outcome)), 11) as tracing_outcome\n" +
-				"                          from kenyaemr_etl.etl_peer_tracking t\n" +
-				"                          where t.visit_date <= date(:endDate) and t.attempt_number > 1\n" +
-				"                          group by t.client_id) t on f.patient_id = t.client_id\n" +
-				"      where f.visit_date <= date(:endDate)\n" +
-				"      group by f.patient_id) a;";
+		String qry = "select a.patient_id, if(a.tracing_outcome <> '', 'Y', 'N')\n"
+		        + "from (select f.patient_id,\n"
+		        + "             max(date(f.visit_date))                                     as latest_fup_visit,\n"
+		        + "             mid(max(concat(f.visit_date, f.next_appointment_date)), 11) as latest_app_date,\n"
+		        + "             t.tracking_date,\n"
+		        + "             t.tracing_outcome\n"
+		        + "      from kenyaemr_etl.etl_patient_hiv_followup f\n"
+		        + "               left join (select t.client_id,\n"
+		        + "                                 max(t.visit_date)                                     as tracking_date,\n"
+		        + "                                 mid(max(concat(t.visit_date, t.tracing_outcome)), 11) as tracing_outcome\n"
+		        + "                          from kenyaemr_etl.etl_peer_tracking t\n"
+		        + "                          where t.visit_date <= date(:endDate) and t.attempt_number > 1\n"
+		        + "                          group by t.client_id) t on f.patient_id = t.client_id\n"
+		        + "      where f.visit_date <= date(:endDate)\n" + "      group by f.patient_id) a;";
 		
 		SqlQueryBuilder queryBuilder = new SqlQueryBuilder();
 		Date startDate = (Date) context.getParameterValue("startDate");
