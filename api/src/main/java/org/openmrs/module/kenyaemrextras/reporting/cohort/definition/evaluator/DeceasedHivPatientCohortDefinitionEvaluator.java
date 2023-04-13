46eee1468836512759c2ff928a88932433784b43
njorocs <njorocs@gmail.com>
2023-04-13 12:51:47 +0300

    Added TB variables in HIV and TB co-infected

--- api/src/main/java/org/openmrs/module/kenyaemrextras/reporting/cohort/definition/evaluator/DeceasedHivPatientCohortDefinitionEvaluator.java
+++ api/src/main/java/org/openmrs/module/kenyaemrextras/reporting/cohort/definition/evaluator/DeceasedHivPatientCohortDefinitionEvaluator.java
@@ -77,7 +77,7 @@ public class DeceasedHivPatientCohortDefinitionEvaluator implements CohortDefini
 		        + "          where coalesce(date(d.date_died),date(d.effective_discontinuation_date), date(d.visit_date)) between\n"
 		        + "          date(:startDate)\n"
 		        + "          and date(:endDate)\n"
-		        + "          and d.discontinuation_reason = 160034\n"
+		        + "          and d.discontinuation_reason = 160034 and d.program_name = 'HIV'\n"
 		        + "          and ((tb.tb_patient is null or tb.tb_enrollment_date < tb.tb_disc_date or\n"
 		        + "          timestampdiff(MONTH, tb.tb_enrollment_date,coalesce(date(d.date_died),date(d.effective_discontinuation_date),date(d.visit_date))) > 10) and s.patient_id is null\n"
 		        + "          ) group by d.patient_id;";
